cee28d241f9e72b5bbc9315d85e34585744fd21f
Surubuey <ignaciomiller@gmail.com>
2024-08-15 21:06:27 -0300

    CREATED incidence calculations, added input controls for user to select paramen angle and parasol depth ratio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,114 @@
+import CircularSlider from "react-circular-slider-svg";
 import YearTable from "./components/YearTable";
+import { useMemo, useState } from "react";
 
 function App() {
+  const [normalAz, setNormalAz] = useState(45);
+  const [normalAlt, setNormalAlt] = useState(0);
+  const [verticalDepth, setVerticalDepth] = useState(0.6);
+  const [horizontalDepth, setHorizontalDepth] = useState(0.6);
+
+  const normal = useMemo(() => {
+    return {
+      az: normalAz, // (normalAz * Math.PI) / 180,
+      alt: normalAlt, // ((normalAlt) * Math.PI) / 180,
+    };
+  }, [normalAz, normalAlt]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <YearTable />
-      </header>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ position: "relative" }}>
+          <label
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            {Math.round(normalAz)}°
+          </label>
+          <CircularSlider
+            size={200}
+            trackWidth={4}
+            minValue={-180}
+            maxValue={180}
+            startAngle={0}
+            endAngle={360}
+            angleType={{
+              direction: "cw",
+              axis: "-y",
+            }}
+            handle1={{
+              value: normalAz,
+              onChange: (v) => setNormalAz(v),
+            }}
+            arcColor="lime"
+            arcBackgroundColor="lime"
+          />
+        </div>
+        <div style={{ position: "relative" }}>
+          <label
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            {Math.round(normalAlt)}°
+          </label>
+          <CircularSlider
+            size={200}
+            trackWidth={4}
+            minValue={0}
+            maxValue={180}
+            startAngle={90}
+            endAngle={270}
+            angleType={{
+              direction: "ccw",
+              axis: "-y",
+            }}
+            handle1={{
+              value: normalAlt,
+              onChange: (v) => setNormalAlt(v),
+            }}
+            arcColor="lime"
+            arcBackgroundColor="lime"
+          />
+        </div>
+        <div>
+          <label>Vert: {parseFloat(verticalDepth).toFixed(3)}</label>
+          <input
+            type="range"
+            min={0}
+            step={0.1}
+            max={3}
+            value={verticalDepth}
+            onChange={(e) => setVerticalDepth(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Hor: {parseFloat(horizontalDepth).toFixed(3)}</label>
+          <input
+            type="range"
+            min={0}
+            step={0.1}
+            max={3}
+            value={horizontalDepth}
+            onChange={(e) => setHorizontalDepth(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <YearTable
+        normal={normal}
+        stepX={7}
+        stepY={0.5}
+        verticalDepth={verticalDepth}
+        horizontalDepth={horizontalDepth}
+      />
     </div>
   );
 }
--- src/components/YearTable.js
+++ src/components/YearTable.js
@@ -4,7 +4,13 @@ import SunCalc from "suncalc3";
 const LAT = -34.9053128;
 const LNG = -56.1505879;
 
-export default function YearTable({ stepX = 3.5, stepY = 0.5 }) {
+export default function YearTable({
+  stepX = 3.5,
+  stepY = 0.5,
+  normal = { az: 0, alt: Math.HALF_PI },
+  verticalDepth = 1,
+  horizontalDepth = 1,
+}) {
   const cells = useMemo(() => {
     const cells = [];
     for (let hour = 0; hour < 24; hour += stepY) {
@@ -17,21 +23,49 @@ export default function YearTable({ stepX = 3.5, stepY = 0.5 }) {
           date.setHours(Math.floor(hour));
           date.setMinutes((hour % 1) * 60);
           const sunPosition = SunCalc.getPosition(date, LAT, LNG);
+          const az = (sunPosition.azimuth * 180) / Math.PI;
+          const alt = (sunPosition.altitude * 180) / Math.PI;
+          const relativeAzRadians =
+            sunPosition.azimuth - (normal.az / 180) * Math.PI;
+          const relativeAltRadians =
+            sunPosition.altitude - (normal.alt / 180) * Math.PI;
+          const relativeAz = az - normal.az;
+          const relativeAlt = alt - normal.alt;
+          const normalAltRadians = (normal.alt / 180) * Math.PI;
+          // θu,v=cos−1[cos(El1)cos(El2)cos(Az1−Az2)+sin(El1)sin(El2)].
+          const relAngle = Math.acos(
+            Math.cos(normalAltRadians) *
+              Math.cos(sunPosition.altitude) *
+              Math.cos(relativeAzRadians) +
+              Math.sin(normalAltRadians) * Math.sin(sunPosition.altitude)
+          );
+          const shadowHorizontalAlt =
+            Math.tan(relAngle) < 0
+              ? 1
+              : Math.min(1, Math.tan(relAngle) * horizontalDepth);
+          const shadowVerticalAlt = Math.tan(relativeAzRadians) * verticalDepth;
           row.push({
             date,
-            az: (sunPosition.azimuth * 180) / Math.PI,
-            alt: (sunPosition.altitude * 180) / Math.PI,
+            az,
+            alt,
+            relativeAz,
+            relativeAlt,
+            shadowHorizontalAlt,
+            shadowVerticalAlt,
+            relAngle,
+            relAngleGrad: (relAngle * 180) / Math.PI,
           });
         }
         cells.push(row);
       }
     }
     return cells;
-  }, [stepX, stepY]);
+  }, [stepX, stepY, normal, horizontalDepth, verticalDepth]);
 
   return (
-    <table style={{ borderCollapse: "collapse" }}>
-      {/* <thead>
+    <>
+      <table style={{ borderCollapse: "collapse" }}>
+        {/* <thead>
         <tr>
           <th />
           {cells[0].map((cell, index) => (
@@ -41,28 +75,33 @@ export default function YearTable({ stepX = 3.5, stepY = 0.5 }) {
           ))}
         </tr>
       </thead> */}
-      <tbody>
-        {cells.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {/* <th style={{ textAlign: "right" }}>
+        <tbody>
+          {cells.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {/* <th style={{ textAlign: "right" }}>
               {row[0].date.getHours()}:
               {("0" + row[0].date.getMinutes()).slice(-2)}
             </th> */}
-            {row.map((cell, cellIndex) => (
-              <Cell key={cellIndex} {...cell} stepX={stepX} stepY={stepY} />
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+              {row.map((cell, cellIndex) => (
+                <Cell key={cellIndex} {...cell} stepX={stepX} stepY={stepY} />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
 function Cell(cell) {
-  const hue = Math.max(0, Math.min(360, 270 - Math.max(0, cell.alt) * 4));
-  const sat = 100 + (50 * cell.alt) / 90 + 50;
-  const lum = (50 * cell.alt) / 90;
-  const backgroundColor = `hsl(${hue}deg ${sat}% ${lum}%)`; // hsl(0deg 100% 50%)
+  const relAngle = 90 - cell.relAngleGrad;
+  const hue = Math.max(0, Math.min(360, 270 - Math.max(0, relAngle) * 4));
+  const sat = 100 + (50 * relAngle) / 90 + 50;
+  const lum = (50 * relAngle) / 90;
+  const backgroundColor =
+    Math.abs(relAngle) < 90 && cell.alt > 0
+      ? `hsl(${hue}deg ${sat}% ${lum}%)`
+      : "black"; // hsl(0deg 100% 50%)
 
   return (
     <td
@@ -71,22 +110,53 @@ function Cell(cell) {
         // border: "1px solid #888",
         textAlign: "center",
         backgroundColor,
+        position: "relative",
       }}
-      title={cell.date.toLocaleString("es-UY", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        timeZone: "America/Montevideo",
-      })}
+      title={
+        cell.date.toLocaleString("es-UY", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+          timeZone: "America/Montevideo",
+        }) +
+        ` ${cell.az.toFixed(2)}° ${cell.alt.toFixed(
+          2
+        )}° ${cell.relativeAz.toFixed(2)}° ${cell.relAngleGrad.toFixed(
+          2
+        )}° ${relAngle}`
+      }
     >
       <div
         style={{
           minWidth: `calc(100vw * ${cell.stepX} / 365)`,
           minHeight: `calc(100vh * ${cell.stepY} / 24)`,
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: `calc(100vh * ${cell.stepY * cell.shadowHorizontalAlt} / 24)`,
+          background: "rgba(0, 0, 0, 0.5)",
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: `calc(100vh * ${cell.stepY * cell.shadowHorizontalAlt} / 24)`,
+          left: cell.shadowVerticalAlt < 0 ? 0 : "auto",
+          right: cell.shadowVerticalAlt > 0 ? 0 : "auto",
+          width: `calc(100vh * ${
+            cell.stepY * Math.abs(cell.shadowVerticalAlt)
+          } / 24)`,
+          bottom: 0,
+          background: "rgba(0, 0, 0, 0.5)",
         }}
       />
       {/* {cell.alt >= 0 ? `${cell.alt.toFixed(2)}°` : "-"} */}
